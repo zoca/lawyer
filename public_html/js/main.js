9a5a6434039fe80ec4a5d22bd7f56cbb587f37f5
Zorica <zorica.mijatovic@cubes.rs>
2019-10-25 11:05:59 +0200

    contact page done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -41,4 +41,45 @@ $(document).ready(function(){
             }
         });
     }
+
+    //form validate
+     if ($('.contact-form')) {
+        $('.contact-form').validate({
+            highlight: function (element) {
+                $(element).addClass('is-invalid').removeClass('is-valid');
+            },
+            unhighlight: function (element) {
+                $(element).addClass('is-valid').removeClass('is-invalid');
+            },
+            rules: {
+                name: {
+                    required: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                }
+            },
+            messages:{
+                name:{
+                    required: 'The Name* field is required.'             
+                 },
+                 email:{
+                    required: 'The Email* field is required.',
+                    email: 'Please provide a valid email address'
+                 },
+                 message:{
+                     required: 'The Message* field is required.'             
+                  },
+            },
+            errorElement: 'p',
+            errorPlacement: function(error, element) {
+                error.appendTo( element.closest(".form-group").find(".error-msg") );
+            }
+
+        });
+    }
 });
